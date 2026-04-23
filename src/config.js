5a5b85f74f0b7e74901b28d4e2118524f722a1ef
Bill Katz <billkatz@gmail.com>
2026-04-23 00:46:31 -0400

    modify OAuth2 to new GCP project

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 export default {
   // project: 'FlyEM Private',
-  project: 'clio-294722',
+  project: 'janeliacave-global',
   projectBaseUrlDefault: 'https://clio-store-vwzoicitea-uk.a.run.app',
-  projectBaseUrlTest: 'https://clio-store-694445642738.us-east4.run.app/',
+  projectBaseUrlTest: 'https://clio-dev.janelia.org:8080',
   top_level_function: 'v2',
   imageSliceUrlTemplate: 'https://tensorslice-bmcp5imp6q-uk.a.run.app/slice/<xyz>/256_256_1/jpeg?location=<location>',
   google_auth: {
-    // client_id: '833853795110-2eu65hnvthhcibk64ibftemb0i1tlu97.apps.googleusercontent.com',
-    client_id: '464281314980-qlc4o2ce8htnhhgdb0g0j681de2l3980.apps.googleusercontent.com',
+    client_id: '603632814084-su0abefiti02gm98ku5f2jlpfdja88bp.apps.googleusercontent.com',
     fetch_basic_profile: true,
     // need this scope to access google cloud storage buckets
     // scope: 'https://www.googleapis.com/auth/devstorage.read_only',
